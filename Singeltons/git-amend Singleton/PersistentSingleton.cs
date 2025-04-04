245cecc597062e5ce5466c9f2d925316aeb53ae5
Grandtheftzebra <149605812+Grandtheftzebra@users.noreply.github.com>
2025-04-04 22:20:39 +0200

    Update PersistentSingleton.cs

--- Singeltons/git-amend Singleton/PersistentSingleton.cs
+++ Singeltons/git-amend Singleton/PersistentSingleton.cs
@@ -2,9 +2,9 @@
 
 using UnityEngine;
 
-namespace UnityUtils 
+namespace UnityUtils
 {
-    public class PersistentSingleton<T> : MonoBehaviour where T : Component 
+    public class PersistentSingleton<T> : MonoBehaviour where T : Component
     {
         public bool AutoUnparentOnAwake = true;
 
@@ -13,13 +13,14 @@ namespace UnityUtils
         public static bool HasInstance => instance != null;
         public static T TryGetInstance() => HasInstance ? instance : null;
 
-        public static T Instance 
+        public static T Instance
         {
-            get {
-                if (instance == null) 
+            get
+            {
+                if (instance == null)
                 {
                     instance = FindAnyObjectByType<T>();
-                    if (instance == null) 
+                    if (instance == null)
                     {
                         var go = new GameObject(typeof(T).Name + " Auto-Generated");
                         instance = go.AddComponent<T>();
@@ -34,20 +35,20 @@ namespace UnityUtils
         /// Make sure to call base.Awake() in override if you need awake.
         /// </summary>
         protected virtual void Awake() => InitializeSingleton();
-        
-        protected virtual void InitializeSingleton() 
+
+        protected virtual void InitializeSingleton()
         {
             if (!Application.isPlaying) return;
 
             if (AutoUnparentOnAwake) transform.SetParent(null);
-            
 
-            if (instance == null) 
+
+            if (instance == null)
             {
                 instance = this as T;
                 DontDestroyOnLoad(gameObject);
-            } 
-            else 
+            }
+            else
             {
                 if (instance != this) Destroy(gameObject);
             }
